06a89e5dce93f157a0a0dfff06137aeeedc3b547
Toby Cortez <Tcortez@fullsail.edu>
2016-06-30 20:46:30 -0700

    now with even more cowbell
    Added Database to store login creds

--- scottyint.py
+++ scottyint.py
@@ -8,27 +8,6 @@ import random
 import items
 import sqlite3
 
-row=25
-column=110
-attempts=0
-
-# Set the Terminal window size larger than its default!
-sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=row,cols=column))
-
-# Scottybot auth info
-auth = {
-	"event": "auth",
-	"data": "<SCOTTYBOT_AUTH_CODE>"
-}
-
-server = {
-	'host': '<TELNET_HOST>',
-	'port': '<PORT>',
-	'password': '<PASSWORD>',
-	'username': '<PLAYER_NAME>'
-}
-
-
 # set to True to debug
 debug = False
 
@@ -61,14 +40,12 @@ commands = [
 # ##########################   NO EDIT BELOW THIS LINE   ######################## #
 ###################################################################################
 
-# 7 Days to Die Player Name
-steam = server['username']
+row = 25
+column = 110
+attempts = 0
 
-# Scotty Bot sub command
-sub = {
-	"event": "subscribe",
-	"data": "commands"
-}
+# Set the Terminal window size larger than its default!
+sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=row, cols=column))
 
 '''
 Items have variable amounts
@@ -109,7 +86,8 @@ def on_message(ws, message):
 			data.append(response['data']['rawcommand'])
 			data.append(response['data']['username'])
 			data.append(response['data']['userid'])
-			#print(data)
+			if debug:
+				print(data)
 
 			# Spawn Animal
 			if data[0] == commands[5]:
@@ -118,31 +96,31 @@ def on_message(ws, message):
 			# Spawn Tool
 			elif data[0] == commands[1]:
 				give(tools)
-				
+
 			# Spawn Clothes
 			elif data[0] == commands[2]:
 				give(clothes)
-				
+
 			# Spawn Explosive
 			elif data[0] == commands[9]:
 				give(explosives)
-				
+
 			# Spawn Weapon
 			elif data[0] == commands[8]:
 				give(weapons)
-				
+
 			# Spawn Health
 			elif data[0] == commands[3]:
 				give(health)
-				
+
 			# Spawn Food
 			elif data[0] == commands[4]:
 				give(food)
-				
+
 			# Spawn Quest
 			elif data[0] == commands[6]:
 				give(quests)
-				
+
 			# Spawn Enemy
 			elif data[0] == commands[10]:
 
@@ -151,7 +129,7 @@ def on_message(ws, message):
 				else:
 					entity(zombies)
 
-			# Spawn Item 
+			# Spawn Item
 			elif data[0] == commands[0]:
 				# **LESS CHANCE OF AIRDROP THAN BEFORE**
 				if num == 73:
@@ -178,6 +156,8 @@ def on_message(ws, message):
 
 def give(enttype):
 	key = random.randrange(0, len(enttype))
+	if debug:
+		print(enttype)
 	if enttype == 'spawnairdrop':
 		os.system('python {} {} {} {} 1 spawnairdrop'.format(pyscript_path, server['host'], server['port'], server['password']))
 	else:
@@ -186,7 +166,8 @@ def give(enttype):
 
 def entity(enttype):
 	key = random.randrange(0, len(enttype))
-	print(enttype)
+	if debug:
+		print(enttype)
 	if enttype == 'zombieFeral' or enttype == 'zombieScreamer':
 		os.system('python {} {} {} {} 2 spawnentity {} {}'.format(pyscript_path, server['host'], server['port'], server['password'], server['username'], enttype))
 	elif enttype == 'spawnwanderinghorde':
@@ -210,57 +191,11 @@ def on_close(ws):
 # open the connection
 def on_open(ws):
 	def run(*args):
-		conn = sqlite3.connect("interactive.sqlite")
-		cursor = conn.cursor()
-		cursor.execute("""CREATE TABLE IF NOT EXISTS info(
-							auth text NOT NULL, 
-							host text NOT NULL, 
-							port text NOT NULL, 
-							pass text NOT NULL,
-							name text NOT NULL, 
-							first integer)
-						""")
-		def create_db():
-			with conn:
-				cursor.execute('INSERT INTO info VALUES (?,?,?,?,?,?);', (authCode, telnetHost, telnetPort, telnetPass, gameName, 0))
-				conn.commit()			
-				
-		cursor.execute("SELECT count(*) FROM info")
-		info = cursor.fetchone()[0]
-		
-			
-		if info == 0:			
-			print('''
-                                                                                                      
-                  ################################################################						
-                  ##                                                            ##
-                  ##                WELCOME TO 7DTD INTERACTIVE:                ##
-                  ##                                                            ##
-                  ##             THIS APPEARS TO BE YOUR FIRST RUN              ##
-                  ##                                                            ##
-                  ##         ENTER YOUR INFO INTO THE FOLLOWING PROMPTS         ##
-                  ##                                                            ##
-                  ################################################################
-                  
-                  
-        ''')
-			time.sleep(3)
-				
-			authCode = input("Scotty Bot Auth Code: ")
-			telnetHost = input("Enter 7DTD Telnet Host: ")
-			telnetPort = input("Enter Telnet Server Port: ")
-			telnetPass = input("Enter Telnet Password: ")
-			gameName = input("Enter 7DTD Player Name: ")		
-			create_db()
-			
-        
-		cursor.execute("SELECT * FROM info")
-		infoList = cursor.fetchall()
-		print("infoList - ", infoList, "\n")            
-		
+		# Scottybot auth info
+
 		print('''
-                                                                                                      
-                  ################################################################						
+
+                  ################################################################
                   ##                                                            ##
                   ##                  TO CLOSE THIS APPLICATION:                ##
                   ##                                                            ##
@@ -274,14 +209,14 @@ def on_open(ws):
                   ##                                                            ##
                   ##                                                            ##
                   ################################################################
-                  
-                  
+
+
         ''')
-		
+
 		time.sleep(3)
-		
+
 		print('''
-						
+
                   ################################################################
                   ##                                                            ##
                   ##                         Credits:                           ##
@@ -295,42 +230,42 @@ def on_open(ws):
                   ##      Coder5452 ------------ Headaches and Complaining      ##
                   ##                                                            ##
                   ################################################################
-                  
-                  
+
+
 		''')
-		
+
 		time.sleep(3)
-		
+
 		print('''
-		                                                                                                      
-  @@@@@@@@     @@@@@@@    @@@@@@   @@@ @@@   @@@@@@      @@@@@@@   @@@@@@      @@@@@@@   @@@  @@@@@@@@  
-  @@@@@@@@     @@@@@@@@  @@@@@@@@  @@@ @@@  @@@@@@@      @@@@@@@  @@@@@@@@     @@@@@@@@  @@@  @@@@@@@@  
-       @@!     @@!  @@@  @@!  @@@  @@! !@@  !@@            @@!    @@!  @@@     @@!  @@@  @@!  @@!       
-      !@!      !@!  @!@  !@!  @!@  !@! @!!  !@!            !@!    !@!  @!@     !@!  @!@  !@!  !@!       
-     @!!       @!@  !@!  @!@!@!@!   !@!@!   !!@@!!         @!!    @!@  !@!     @!@  !@!  !!@  @!!!:!    
-    !!!        !@!  !!!  !!!@!!!!    @!!!    !!@!!!        !!!    !@!  !!!     !@!  !!!  !!!  !!!!!:    
-   !!:         !!:  !!!  !!:  !!!    !!:         !:!       !!:    !!:  !!!     !!:  !!!  !!:  !!:       
-  :!:          :!:  !:!  :!:  !:!    :!:        !:!        :!:    :!:  !:!     :!:  !:!  :!:  :!:       
-   ::           :::: ::  ::   :::     ::    :::: ::         ::    ::::: ::      :::: ::   ::   :: ::::  
-  : :          :: :  :    :   : :     :     :: : :          :      : :  :      :: :  :   :    : :: ::   
-                                                                                                      
-                                                                                                      
-     @@@  @@@  @@@  @@@@@@@  @@@@@@@@  @@@@@@@    @@@@@@    @@@@@@@  @@@@@@@  @@@  @@@  @@@  @@@@@@@@   
-     @@@  @@@@ @@@  @@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@  @@@  @@@  @@@  @@@@@@@@   
-     @@!  @@!@!@@@    @@!    @@!       @@!  @@@  @@!  @@@  !@@         @@!    @@!  @@!  @@@  @@!        
-     !@!  !@!!@!@!    !@!    !@!       !@!  @!@  !@!  @!@  !@!         !@!    !@!  !@!  @!@  !@!        
-     !!@  @!@ !!@!    @!!    @!!!:!    @!@!!@!   @!@!@!@!  !@!         @!!    !!@  @!@  !@!  @!!!:!     
-     !!!  !@!  !!!    !!!    !!!!!:    !!@!@!    !!!@!!!!  !!!         !!!    !!!  !@!  !!!  !!!!!:     
-     !!:  !!:  !!!    !!:    !!:       !!: :!!   !!:  !!!  :!!         !!:    !!:  :!:  !!:  !!:        
-     :!:  :!:  !:!    :!:    :!:       :!:  !:!  :!:  !:!  :!:         :!:    :!:   ::!!:!   :!:        
-      ::   ::   ::     ::     :: ::::  ::   :::  ::   :::   ::: :::     ::     ::    ::::     :: ::::   
-     :    ::    :      :     : :: ::    :   : :   :   : :   :: :: :     :     :       :      : :: ::  
+
+  @@@@@@@@     @@@@@@@    @@@@@@   @@@ @@@   @@@@@@      @@@@@@@   @@@@@@      @@@@@@@   @@@  @@@@@@@@
+  @@@@@@@@     @@@@@@@@  @@@@@@@@  @@@ @@@  @@@@@@@      @@@@@@@  @@@@@@@@     @@@@@@@@  @@@  @@@@@@@@
+       @@!     @@!  @@@  @@!  @@@  @@! !@@  !@@            @@!    @@!  @@@     @@!  @@@  @@!  @@!
+      !@!      !@!  @!@  !@!  @!@  !@! @!!  !@!            !@!    !@!  @!@     !@!  @!@  !@!  !@!
+     @!!       @!@  !@!  @!@!@!@!   !@!@!   !!@@!!         @!!    @!@  !@!     @!@  !@!  !!@  @!!!:!
+    !!!        !@!  !!!  !!!@!!!!    @!!!    !!@!!!        !!!    !@!  !!!     !@!  !!!  !!!  !!!!!:
+   !!:         !!:  !!!  !!:  !!!    !!:         !:!       !!:    !!:  !!!     !!:  !!!  !!:  !!:
+  :!:          :!:  !:!  :!:  !:!    :!:        !:!        :!:    :!:  !:!     :!:  !:!  :!:  :!:
+   ::           :::: ::  ::   :::     ::    :::: ::         ::    ::::: ::      :::: ::   ::   :: ::::
+  : :          :: :  :    :   : :     :     :: : :          :      : :  :      :: :  :   :    : :: ::
+
+
+     @@@  @@@  @@@  @@@@@@@  @@@@@@@@  @@@@@@@    @@@@@@    @@@@@@@  @@@@@@@  @@@  @@@  @@@  @@@@@@@@
+     @@@  @@@@ @@@  @@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@  @@@  @@@  @@@  @@@@@@@@
+     @@!  @@!@!@@@    @@!    @@!       @@!  @@@  @@!  @@@  !@@         @@!    @@!  @@!  @@@  @@!
+     !@!  !@!!@!@!    !@!    !@!       !@!  @!@  !@!  @!@  !@!         !@!    !@!  !@!  @!@  !@!
+     !!@  @!@ !!@!    @!!    @!!!:!    @!@!!@!   @!@!@!@!  !@!         @!!    !!@  @!@  !@!  @!!!:!
+     !!!  !@!  !!!    !!!    !!!!!:    !!@!@!    !!!@!!!!  !!!         !!!    !!!  !@!  !!!  !!!!!:
+     !!:  !!:  !!!    !!:    !!:       !!: :!!   !!:  !!!  :!!         !!:    !!:  :!:  !!:  !!:
+     :!:  :!:  !:!    :!:    :!:       :!:  !:!  :!:  !:!  :!:         :!:    :!:   ::!!:!   :!:
+      ::   ::   ::     ::     :: ::::  ::   :::  ::   :::   ::: :::     ::     ::    ::::     :: ::::
+     :    ::    :      :     : :: ::    :   : :   :   : :   :: :: :     :     :       :      : :: ::
    ''')
-   	
+
 		# send the auth and sub data
 		ws.send(json.dumps(auth))
 		ws.send(json.dumps(sub))
-		
+
 		# keep sending to keep the connection open
 		while True:
 			time.sleep(10)
@@ -341,7 +276,7 @@ def on_open(ws):
 if __name__ == "__main__":
 	if debug:
 		websocket.enableTrace(True)
-	
+
 	if len(sys.argv) < 2:
 		host = "wss://api.scottybot.net/websocket/control"
 	else:
@@ -350,6 +285,77 @@ if __name__ == "__main__":
 					on_message=on_message,
 					on_error=on_error,
 					on_close=on_close)
+
+	conn = sqlite3.connect("interactive.sqlite")
+	cursor = conn.cursor()
+	cursor.execute("""CREATE TABLE IF NOT EXISTS info(
+						auth text NOT NULL,
+						host text NOT NULL,
+						port text NOT NULL,
+						pass text NOT NULL,
+						name text NOT NULL,
+						first integer)
+					""")
+
+	def create_db():
+		with conn:
+			cursor.execute('INSERT INTO info VALUES (?,?,?,?,?,?);', (authCode, telnetHost, telnetPort, telnetPass, gameName, 0))
+			conn.commit()
+
+	cursor.execute("SELECT count(*) FROM info")
+	info = cursor.fetchone()[0]
+
+	if info == 0:
+		print('''
+
+              ################################################################
+              ##                                                            ##
+              ##                WELCOME TO 7DTD INTERACTIVE:                ##
+              ##                                                            ##
+              ##             THIS APPEARS TO BE YOUR FIRST RUN              ##
+              ##                                                            ##
+              ##         ENTER YOUR INFO INTO THE FOLLOWING PROMPTS         ##
+              ##                                                            ##
+              ################################################################
+
+
+    ''')
+		time.sleep(3)
+
+		authCode = input("Scotty Bot Auth Code: ")
+		telnetHost = input("Enter 7DTD Telnet Host: ")
+		telnetPort = input("Enter Telnet Server Port: ")
+		telnetPass = input("Enter Telnet Password: ")
+		gameName = input("Enter 7DTD Player Name: ")
+		create_db()
+
+	cursor.execute("SELECT * FROM info")
+	infoList = cursor.fetchall()
+	if debug:
+		print("infoList - ", infoList, "\n")
+
+	# Scottybot auth info
+	auth = {
+		"event": "auth",
+		"data": infoList[0][0]
+	}
+
+	server = {
+		'host': infoList[0][1],
+		'port': infoList[0][2],
+		'password': infoList[0][3],
+		'username': infoList[0][4]
+	}
+
+	# 7 Days to Die Player Name
+	steam = server['username']
+
+	# Scotty Bot sub command
+	sub = {
+		"event": "subscribe",
+		"data": "commands"
+	}
+
 	ws.on_open = on_open
 
 	ws.run_forever()
